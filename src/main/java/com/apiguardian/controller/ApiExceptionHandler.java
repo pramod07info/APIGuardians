f037dea1e25ece94d9244e3d23e9ee0f0afdba67
pramod07info <68590433+pramod07info@users.noreply.github.com>
2026-09-24 21:56:54 +0530

    Add src/main/java/com/apiguardian/controller/ApiExceptionHandler.java

---
+++ src/main/java/com/apiguardian/controller/ApiExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.apiguardian.controller;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleConstraintViolation(ConstraintViolationException exception) {
+        return Map.of(
+                "error", "Invalid request",
+                "message", exception.getMessage());
+    }
+}
