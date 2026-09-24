1a743093b34424ac27ed7e6bbcfec548fa4c8509
pramod07info <68590433+pramod07info@users.noreply.github.com>
2026-09-24 21:56:50 +0530

    Add src/main/java/com/apiguardian/ApiGuardianApplication.java

---
+++ src/main/java/com/apiguardian/ApiGuardianApplication.java
@@ -0,0 +1,12 @@
+package com.apiguardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGuardianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGuardianApplication.class, args);
+    }
+}
